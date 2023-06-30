c956e5a25bb2d04abdada8e640ddf0596119bace
sparklejoker <132633214+sparklejoker@users.noreply.github.com>
2023-06-30 18:28:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,22 +19,21 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '吴学斌',
+      name: '测试者',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oDCdW6re5QwLECmZk0p8RIp-hMkg',
+      id: 'oDCdW6sFaQYEhbh6cR-ajK79cFbE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '4eUs92HTyjealDe3Y3VD8oJVduirkaMj-f5NNxZv-1g',
+      useTemplateId: 'lp4VC83x-K9d100AbjwGpwDcXNjETrZB28vPrO_WFaY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-06',
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '吴学斌', year: '2002', date: '11-06',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2023', date: '05-20',
+          type: '生日', name: '测试者', year: '2002', date: '11-06',
         },
       ],
+      horoscopeDateType: '今日'
+      
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
@@ -47,13 +46,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'dTls3UklrylgN6zUk8wwqwcdTqAZ8b6HB12anOKiaNQ',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oDCdW6re5QwLECmZk0p8RIp-hMkg',
     }
   ],
 
