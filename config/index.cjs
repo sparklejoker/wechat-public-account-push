5650171923e417b7e4a9678365df9643f497bfbe
sparklejoker <132633214+sparklejoker@users.noreply.github.com>
2023-06-30 19:45:16 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -14,8 +14,54 @@ const USER_CONFIG = {
   APP_SECRET: '5cef650a27a323f962ed7745e5e5ce7e',
 
   PROVINCE: '江西',
-  CITY: '抚州',
+  CITY: '黎川',
 
+
+  SWITCH: {
+    weather: true,
+    
+    holidaytts: true,
+    
+    CIBA: false,
+    
+    oneTalk: false,
+    
+    earthyLoveWords: true,
+    
+    momentCopyrighting: false,
+    
+    poisonChickenSoup: false,
+    
+    poetry: false,
+
+    horoscope: false,
+  
+    birthdayMessage: true,
+  
+    courseSchedule: false,
+  },
+
+  TIAN_API: {
+    key: '',
+    
+    
+    morningGreeting: false,
+  
+    
+    eveningGreeting: false,
+  
+    
+    weather: 3,
+  
+    
+    networkHot: 0,
+  
+    
+    networkHotType: 'default',
+  },
+
+  IS_SHOW_COLOR: false,
+  
   USERS: [
     {
       // 想要发送的人的名字
@@ -34,12 +80,24 @@ const USER_CONFIG = {
       ],
       horoscopeDateType: '今日',
       
+      SLOT_LIST: [
+    // 这样配置的话，就会每次随机选一句话发送
+    {
+      keyword: 'lover_prattle',
+      contents: [
+        '因为太喜欢你，所以看谁都像是情敌。',
+        '申请成为你爱里的永久居民。',
+        '你很傻，你很笨，可我还是很羡慕你，因为你有我',
+        '遇见你，就好像捡到了100斤的运气'
+      ],
+    }
+    // 你可以不断按格式往下增加
+    // ...
+  ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2023-05-20' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
